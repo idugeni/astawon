4e10b3c082d05af9177aad6efac0ad7af1357ae0
Jagad Brahma Wiraatmaja <officialelsa21@gmail.com>
2025-02-09 03:33:02 +0700

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "eslint:recommended", // Tambahkan aturan dasar ESLint
-    "plugin:@typescript-eslint/recommended", // Tambahkan aturan khusus TypeScript
-    "plugin:prettier/recommended" // Integrasi Prettier dengan ESLint
-  ),
-  {
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      // Aturan kustom untuk meningkatkan best practice
-      "no-console": "warn", // Peringatan jika ada console.log
-      "react/no-unescaped-entities": "off", // Izinkan penggunaan karakter seperti ' & "
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // Hindari error pada variabel yang tidak terpakai
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
-      // Aturan tambahan untuk meningkatkan kode
-      "prettier/prettier": ["error", { "singleQuote": true, "semi": false }], // Gunakan single quote, tanpa titik koma
-      "react-hooks/rules-of-hooks": "error", // Pastikan hooks digunakan dengan benar
-      "react-hooks/exhaustive-deps": "warn", // Peringatan jika ada dependensi useEffect yang kurang lengkap
-
-      "quotes": ["error", "single"], // Pakai tanda ' (single quote)
-      "jsx-quotes": ["error", "prefer-single"] // Pakai ' dalam TSX
-    }
-  }
-];
-
-export default eslintConfig;
+export default eslintConfig
